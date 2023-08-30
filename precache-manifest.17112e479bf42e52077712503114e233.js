c3711ba7065ff5ef33c4c7efde3b903b67edd5d0
Niyaj-Kumanali <iamniyazahmad777@gmail.com>
2023-08-30 23:48:12 +0530

    Updates

---
+++ precache-manifest.17112e479bf42e52077712503114e233.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "925ffff8775e19b9153d",
+    "url": "/monsters_rolodex_reactjs/static/css/main.7f7357f7.chunk.css"
+  },
+  {
+    "revision": "925ffff8775e19b9153d",
+    "url": "/monsters_rolodex_reactjs/static/js/main.925ffff8.chunk.js"
+  },
+  {
+    "revision": "1ae1efd2b996e159ba79",
+    "url": "/monsters_rolodex_reactjs/static/js/1.1ae1efd2.chunk.js"
+  },
+  {
+    "revision": "d96758fe2eaf9bdefff3",
+    "url": "/monsters_rolodex_reactjs/static/js/runtime~main.d96758fe.js"
+  },
+  {
+    "revision": "51571c5be9556f054e0b980870e853d4",
+    "url": "/monsters_rolodex_reactjs/index.html"
+  }
+];
